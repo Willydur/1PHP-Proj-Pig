420867e9e85cb00f187f335dcab6e91f54f96520
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2023-03-16 15:45:29 +0100

    k

--- panier.php
+++ panier.php
@@ -2,12 +2,7 @@
 session_start();
 if (isset($_GET['film'])) {
   $id = $_GET['film'];
-  if (isset($_SESSION['cart'][$id])) {
-    $_SESSION['cart'][$id]++;
-  } else {
-    $_SESSION['cart'][$id] = 1;
-  }
-  var_dump($_SESSION);
+  $_SESSION['cart'][] = $id;
   $db = new PDO("mysql:host=eliascastel.ddns.net;dbname=php1Pig", "php1", "SupInfo2023!");
   $a = implode(",", $_SESSION['cart']);
   $b = $_SESSION['name'];
@@ -15,6 +10,21 @@ if (isset($_GET['film'])) {
   $res->bindParam(1, $a);
   $res->bindParam(2, $b);
   $res->execute();
+  sleep(1);
+  header("Location:panier.php");
+}
+if (isset($_GET['supprimer'])){
+  $id = $_GET['supprimer'];
+  unset($_SESSION['cart'][$id]);
+  $db = new PDO("mysql:host=eliascastel.ddns.net;dbname=php1Pig", "php1", "SupInfo2023!");
+  $a = implode(",", $_SESSION['cart']);
+  $b = $_SESSION['name'];
+  $res = $db->prepare("UPDATE user SET cart=? WHERE nom=?");
+  $res->bindParam(1, $a);
+  $res->bindParam(2, $b);
+  $res->execute();
+  sleep(1);
+  header("Location:panier.php");
 }
 ?>
 <!DOCTYPE html>
@@ -77,19 +87,29 @@ if (isset($_GET['film'])) {
       <br>
 
       <center>
+        <h1>Votre panier</h1>
         <div class="grille">
           <?php
           if (!empty($_SESSION['cart'])){
-            $cart = $_SESSION['cart'];
-            foreach ($cart as $un_film) {
-              $db = new PDO("mysql:host=eliascastel.ddns.net;dbname=php1Pig", "php1", "SupInfo2023!");
-              $res = $db->prepare("SELECT * FROM products WHERE id=?");
-              $res->bindParam(1, $un_film);
-              $res->execute();
-              $film = $res->fetch();
-              echo '<div class="grid-item">';
-              echo '<img src="./image/' . $film['img'] . '" >';
-              echo '</div>';
+            if (!empty($_SESSION['cart'][1])){
+              $cart = $_SESSION['cart'];
+              $compteur = 0;
+              foreach ($cart as $un_film) {
+                if ($un_film != "") {
+                  $db = new PDO("mysql:host=eliascastel.ddns.net;dbname=php1Pig", "php1", "SupInfo2023!");
+                  $res = $db->prepare("SELECT * FROM products WHERE id=?");
+                  $res->bindParam(1, $un_film);
+                  $res->execute();
+                  $film = $res->fetch();
+                  echo '<div class="grid-item">';
+                  echo '<img src="./image/' . $film['img'] . '" >';
+                  echo '<a href="panier.php?supprimer=' . $compteur . '">Supprimer</a>';
+                  echo '</div>';
+                }
+                $compteur++;
+              }
+            } else {
+              echo '<h1>Votre panier est vide</h1>';
             }
           } else {
             echo '<h1>Votre panier est vide</h1>';
